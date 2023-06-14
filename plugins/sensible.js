c222af05502da47abfd726aa760558b6e9becde3
Selma999 <selmakovacevic1@hotmail.com>
2023-06-15 00:13:58 +0200

    Setup cors

--- plugins/sensible.js
+++ plugins/sensible.js
@@ -1,6 +1,7 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
+const cors = require("@fastify/cors");
 
 /**
  * This plugins adds some utilities to handle http errors
@@ -8,7 +9,11 @@ const fp = require('fastify-plugin')
  * @see https://github.com/fastify/fastify-sensible
  */
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/sensible'), {
-    errorHandler: false
-  })
-})
+  fastify.register(cors, {
+    origin: "http://127.0.0.1:5173",
+  });
+
+  fastify.register(require("@fastify/sensible"), {
+    errorHandler: false,
+  });
+});

0c8bc321dae7c77a150bcbd163a9d53d6a7d7aca
Selma999 <selmakovacevic1@hotmail.com>
2023-06-15 00:14:23 +0200

    Add endpoint to upload json and save to json file

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,29 @@
-'use strict'
+"use strict";
+
+const fs = require("fs");
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.post("/save-json", async function (request, reply) {
+    // Request body contains json from frontend
+
+    // Extract json from request
+    const { body } = request;
+
+    try {
+      if (!fs.existsSync("files")) {
+        fs.mkdirSync("files");
+      }
+
+      // Save json to file
+      fs.writeFileSync("files/data.json", JSON.stringify(body));
+
+      // Return response if successful
+    } catch (err) {
+      console.log(err);
+
+      return { success: false };
+    }
+
+    return { success: true };
+  });
+};
